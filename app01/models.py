f7868fc7bf9d851b4f372d318936e4d194e54c89
Bean-jun <1342104001@qq.com>
2021-04-07 10:19:18 +0800

    # 删除测试环境 #

--- app01/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class UserInfo(models.Model):
-    username = models.CharField(max_length=32, verbose_name="用户名")
-    email = models.EmailField(max_length=32, verbose_name="邮箱")
-    mobile_phone = models.CharField(max_length=32, verbose_name="手机号")
-    password = models.CharField(max_length=32, verbose_name="密码")
